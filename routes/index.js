3ff5a2c3b97e8ab3c512e0fcefb67a31b7835e22
Jodyburne <j__richardson@live.co.uk>
2019-07-08 20:30:46 +0100

    edit profile working but with definite flaws

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const uploadCloud = require('../config/cloudinary.js');
 const router  = express.Router();
+// const multer  = require('multer');
+const User = require("../models/User");
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+router.get('/edit-profile/:userId', (req, res, next) => {
+  User.findById(req.params.userId)
+  .then(user => {
+      res.render('edit-profile', {user: user});
+
+  })
+});
+
+router.post("/edit-profile/:userId", uploadCloud.single('picture'), (req, res, next) => {
+      // console.log("BUUUUUGGG", req.file)
+    User.findByIdAndUpdate(req.params.userId, {
+    bio: req.body.bio,
+    location: req.body.location,
+    profilePic: req.file.secure_url
+  
+  })
+    .then(user => {
+      // Redirect to the detail page of the item
+      res.redirect("/");
+    });
 });
 
 module.exports = router;
