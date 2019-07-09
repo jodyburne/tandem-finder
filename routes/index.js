7608488f5e3b31ed120776d4107951d95f004715
Jodyburne <j__richardson@live.co.uk>
2019-07-09 18:14:19 +0100

    password hidden

--- routes/index.js
+++ routes/index.js
@@ -9,36 +9,6 @@ const User = require("../models/User");
 router.get('/', (req, res, next) => {
   res.render('index');
 });
-router.get('/edit-profile/:userId', (req, res, next) => {
-  User.findById(req.params.userId)
-  .then(user => {
-      res.render('edit-profile', {user: user});
 
-  })
-});
-
-router.post("/edit-profile/:userId", uploadCloud.single('picture'), (req, res, next) => {
-    
-    let userImg =""
-    if (req.file) {
-      userImg = req.file.secure_url
-    }
-    else {
-      console.log('req undefined')
-      userImg = req.user.profilePic
-    }
-    User.findByIdAndUpdate(req.params.userId, {
-    
-    bio: req.body.bio,
-    nationality: req.body.nationality,
-    dateOfBirth: req.body.dateOfBirth,
-    profilePic: userImg,
-
-  })
-    .then(user => {
-      // Redirect to the detail page of the item
-      res.redirect("/");
-    });
-});
 
 module.exports = router;
